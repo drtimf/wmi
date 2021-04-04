160aba62a6d5ed323d8154fd24117dc082ffcb97
Tim Ferguson <drtimf@gmail.com>
2021-04-04 21:45:03 +1000

    More examples and readme. Add Go module configuration.

--- example/network_adapter.go
+++ example/network_adapter.go
@@ -1,51 +1,3 @@
-# wmi
-A wrapper for local and remote Windows WMI at both low level calls to COM, and at a high level Go object mapping.
-
-There are a number of WMI library implementations around, but not many of them provide:
-* Both local and remote access to the WMI provider
-* A single session to execute many queries
-* Low level access to the WMI API
-* High level mapping of WMI objects to Go objects
-* WMI method execution
-
-This presently only works on Windows.  If there is ever a port of the Python Impacket to Go, it would be good to have
-this work on Linux and MacOS as well.
-
-## Some Examples
-
-### A Simple High-Level Example to Query Win32_ComputerSystem
-
-```go
-package main
-
-import (
-	"fmt"
-
-	"github.com/drtimf/wmi"
-)
-
-func main() {
-	var service *wmi.Service
-	var err error
-
-	if service, err = wmi.NewLocalService(wmi.RootCIMV2); err != nil {
-		panic(err)
-	}
-
-	defer service.Close()
-
-	var computerSystem wmi.Win32_ComputerSystem
-	if err = service.Query("SELECT * FROM Win32_ComputerSystem", &computerSystem); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("%+v\n", computerSystem)
-}
-```
-
-### A Low-Level Example to Query Win32_NetworkAdapter
-
-```go
 package main
 
 import (
@@ -122,6 +74,3 @@ func main() {
 		fmt.Printf("%6d %-25s%20s\t%s\n", interfaceIndex, manufacturer, MACAddress, name)
 	}
 }
-```
-
-
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/drtimf/wmi
+
+go 1.15
+
+require (
+	github.com/go-ole/go-ole v1.2.5
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57
+	gotest.tools v2.2.0+incompatible
+)
